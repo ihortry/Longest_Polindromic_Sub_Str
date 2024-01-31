64152516bb814fac5b7cb637878df1af05500593
Ihor Tryndey <igortryndey@gmail.com>
2024-01-31 02:36:06 +0000

    commit with largestPolindomicSubStr

--- src/leetCode/PolindromicSubStr.java
+++ src/leetCode/PolindromicSubStr.java
@@ -1,12 +1,36 @@
 package leetCode;
 
+import java.util.ArrayList;
+
 public class PolindromicSubStr {
 
 	public static void main(String[] args) {
-		String str = "baabab";
-		System.out.println(simplePolindromic(str));
-	}
+		String str = "madamnfgs";
+		//System.out.println(simplePolindromic(str));
+		System.out.println(largestPolindromicSubStr(str));
+		
 	
+		  
+	}
+
+	private static String largestPolindromicSubStr(String str) {
+		if(str.equals(new StringBuilder(str).reverse().toString()))
+			return str;
+		
+		int maxLength = 1; 
+		String maxStr = str.substring(0,maxLength);
+		
+		for(int i=0; i<str.length();i++) {
+			for(int j = i + maxLength; j < str.length(); j++) {
+				if(j - i > maxLength && simplePolindromic(str.substring(i,j))) {
+					maxLength = j-i;
+					maxStr = str.substring(i,j);
+				}
+			}
+		}
+		return maxStr;
+	}
+
 	public static boolean simplePolindromic(String str) {
 		int start = 0;
 		int end = str.length()-1;
