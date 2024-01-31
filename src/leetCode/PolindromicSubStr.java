e528ce08042ddae67cdae69ed327e2a1bfe62bac
Ihor Tryndey <igortryndey@gmail.com>
2024-01-31 03:12:29 +0000

    commit with largest sequence without repetiotion

--- src/leetCode/PolindromicSubStr.java
+++ src/leetCode/PolindromicSubStr.java
@@ -1,11 +1,12 @@
 package leetCode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class PolindromicSubStr {
 
 	public static void main(String[] args) {
-		String str = "madamnfgs";
+		String str = "aadcccd";
 		//System.out.println(simplePolindromic(str));
 		System.out.println(largestPolindromicSubStr(str));
 		
@@ -13,10 +14,37 @@ public class PolindromicSubStr {
 		  
 	}
 
-	private static char[] largestStrWithNoRepetiotions(String str) {
-		HashSet<Character>
-		return null;
+
+	private static int largestStrWithNoRepetiotions(String str) {
+	
+	  
+			
+		HashSet<Character> c = new HashSet<>();
+		int maxLength = 0;
+		int left = 0;
+		int n = str.length()-1;
+		
+		for(int right = 0; right <= n; right++) {
+			if(!c.contains(str.charAt(right))) {
+				c.add(str.charAt(right));
+				maxLength = Math.max(maxLength, right-left+1);
+			}else {
+				while(c.contains(str.charAt(right))) {
+					System.out.println("right "+ right);
+					System.out.println("1 left "+ left);
+					System.out.println("1: "+ c);
+					c.remove(str.charAt(left));
+					System.out.println("2: "+ c);
+					left++;
+					System.out.println("2++ left "+ left);
+				}
+			}
+			c.add(str.charAt(right));
+		}
+		System.out.println(c);
+		return maxLength;
 	}
+
 
 	private static String largestPolindromicSubStr(String str) {
 		if(str.equals(new StringBuilder(str).reverse().toString()))
